64eabe46bfd17a54819737acd215cc6f8b10e460
Neil Seejoor <nvseejoor@gmail.com>
2018-02-02 23:30:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -7,3 +7,13 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+    
+end
